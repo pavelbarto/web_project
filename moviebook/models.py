632a4d5856f17a7f9bd7da0397ec9a37b1e4d4b8
palino <pavelbarto@gmail.com>
2022-04-12 21:21:08 +0200

    pridanie modelev do suboru models.py

--- moviebook/models.py
+++ moviebook/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Film(models.Model):
+    nazov = models.CharField(max_length=200)
+    rezia = models.CharField(max_length=180)
+
+class Zanr(models.Model):
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+    nazov_zanru = models.CharField(max_length=80)
+
