27637794d8da4c9da492848f3c6a5713651af5d2
palino <pavelbarto@gmail.com>
2022-04-12 21:40:20 +0200

    pridanie databaz

--- moviebook/models.py
+++ moviebook/models.py
@@ -8,7 +8,13 @@ class Film(models.Model):
     nazov = models.CharField(max_length=200)
     rezia = models.CharField(max_length=180)
 
+    def __str__(self):
+        return "Nazov: {0} | Rezia: {1}".format(self.nazov, self.rezia)
+
 class Zanr(models.Model):
     film = models.ForeignKey(Film, on_delete=models.CASCADE)
     nazov_zanru = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "Film: {0} | Nazov_zanru: {1}".format(self.film, self.nazov_zanru)
 
